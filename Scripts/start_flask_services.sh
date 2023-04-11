2d67b788c6dfe2720545743525b8684dfc00a00a
Peter Paprotny <pappet.kn@gmail.com>
2023-04-11 21:56:41 +0200

    First Commit

---
+++ Scripts/start_flask_services.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Start Nginx
+systemctl start nginx
+
+# Change to directory of flask app
+cd /home/flask_app
+
+# Start venv
+source venv/bin/activate
+
+# Start Gunicorn and Flask App
+gunicorn app:app --access-logfile /home/flask_app/logs/gunicorn_access.log --error-logfile /home/flask_app/logs/gunicorn_error.log --log-level info -b 0.0.0.0:8000 --daemon
+

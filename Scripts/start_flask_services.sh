ad97613bea7ccdf39a9cd2cb108c9ab30864c719
Peter Paprotny <pappet.kn@gmail.com>
2023-04-12 20:48:38 +0200

    Changes

--- Scripts/start_flask_services.sh
+++ Scripts/start_flask_services.sh
@@ -7,10 +7,21 @@ systemctl start nginx
 cd /home/flask_website/workspace/flask_website/Application
 
 # Start venv
-python3 -m venv venv
+echo "Activating venv..."
 source venv/bin/activate
 
-# Start Gunicorn and Flask App
+# Start Gunicorn
+echo "Starting Gunicorn..."
 gunicorn app:app --access-logfile /home/flask_app/logs/gunicorn_access.log --error-logfile /home/flask_app/logs/gunicorn_error.log --log-level info -b 0.0.0.0:8000 --daemon
 
+# Wait for Gunicorn to start
+sleep 5
  
+# Check if Gunicorn is running
+GUNICORN_PID=$(pgrep -f "gunicorn app:app")
+
+if [ -n "$GUNICORN_PID" ]; then
+    echo "Gunicorn is running (PID: $GUNICORN_PID)"
+else
+    echo "Gunicorn failed to start"
+fi

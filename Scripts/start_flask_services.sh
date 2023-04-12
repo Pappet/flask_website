8587b13d3630a4d2d2a37aabd1ffd5686cecf7b4
Peter Paprotny <pappet.kn@gmail.com>
2023-04-12 20:28:15 +0200

    Changes

--- Scripts/start_flask_services.sh
+++ Scripts/start_flask_services.sh
@@ -4,7 +4,11 @@
 systemctl start nginx
 
 # Change to directory of flask app
+<<<<<<< HEAD
 cd /home/jenkins/workspace/flask_website
+=======
+cd /home/jenkins/workspace
+>>>>>>> 494506a (Changes)
 
 # Start venv
 source venv/bin/activate
@@ -12,3 +16,4 @@ source venv/bin/activate
 # Start Gunicorn and Flask App
 gunicorn app:app --access-logfile /home/flask_app/logs/gunicorn_access.log --error-logfile /home/flask_app/logs/gunicorn_error.log --log-level info -b 0.0.0.0:8000 --daemon
 
+ 

3a2a3db319564f35fc042e3e3ab875f5e4083b73
Peter Paprotny <pappet.kn@gmail.com>
2023-04-12 21:13:18 +0200

    Changes

--- Scripts/start_flask_services.sh
+++ Scripts/start_flask_services.sh
@@ -9,7 +9,21 @@ cd /home/flask_website/workspace/flask_website/Application
 # Start venv
 echo "Activating venv..."
 python3 -m venv venv
+
 . /home/flask_website/workspace/flask_website/Application/venv/bin/activate
+
+# Wait for venv
+sleep 5
+
+# Check venv status
+if [[ -z "${VIRTUAL_ENV}" ]]; then
+    echo "No virtual environment activated. Aborting script."
+    exit 1
+else
+    echo "Virtual environment activated: ${VIRTUAL_ENV}"
+fi
+
+# Install requirements
 pip install -r /home/flask_website/workspace/flask_website/requirements.txt
 
 
@@ -26,5 +40,6 @@ GUNICORN_PID=$(pgrep -f "gunicorn app:app")
 if [ -n "$GUNICORN_PID" ]; then
     echo "Gunicorn is running (PID: $GUNICORN_PID)"
 else
-    echo "Gunicorn failed to start"
+    echo "Gunicorn failed to start. Aborting script."
+    exit 1
 fi

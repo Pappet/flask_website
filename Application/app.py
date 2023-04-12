67b75cae8d84c181bce35d5afecfe008557b617f
Peter Paprotny <pappet.kn@gmail.com>
2023-04-12 21:30:59 +0200

    Changes

--- Application/app.py
+++ Application/app.py
@@ -5,7 +5,3 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello KIRCHGASSE! Dies ist ein Test!!!'
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)

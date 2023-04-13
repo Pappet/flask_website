5180561e827dff2c5793ebb798b061722ada6477
Peter Paprotny <pappet.kn@gmail.com>
2023-04-13 17:14:26 +0200

    Changes

--- Scripts/start_flask_services.sh
+++ Scripts/start_flask_services.sh
@@ -26,6 +26,7 @@ fi
 # Install requirements
 python3 -m pip install Flask
 python3 -m pip install flask-socketio
+python3 -m pip install eventlet
 # pip install -r /home/flask_website/workspace/flask_website/requirements.txt
 
 

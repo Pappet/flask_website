f968fa0a8050f25ade7e7b3fb68b80665eebc99a
Peter Paprotny <pappet.kn@gmail.com>
2023-04-13 17:34:00 +0200

    Changes

--- Scripts/start_flask_services.sh
+++ Scripts/start_flask_services.sh
@@ -7,26 +7,26 @@ systemctl start nginx
 cd /home/jenkins/workspace/flask_website/Application
 
 # Start venv
-echo "Activating venv..."
-python3 -m venv venv
+# echo "Activating venv..."
+# python3 -m venv venv
 
-. /home/jenkins/workspace/flask_website/Application/venv/bin/activate
+# . /home/jenkins/workspace/flask_website/Application/venv/bin/activate
 
 # Wait for venv
 sleep 5
 
 # Check venv status
-if [[ -z "${VIRTUAL_ENV}" ]]; then
-    echo "No virtual environment activated. Aborting script."
-    exit 1
-else
-    echo "Virtual environment activated: ${VIRTUAL_ENV}"
-fi
+# if [[ -z "${VIRTUAL_ENV}" ]]; then
+#     echo "No virtual environment activated. Aborting script."
+#     exit 1
+# else
+#     echo "Virtual environment activated: ${VIRTUAL_ENV}"
+# fi
 
 # Install requirements
 python3 -m pip install Flask
 python3 -m pip install flask_socketio
-python3 -m pip install eventlet
+python3 -m pip install gunicorn==20.1.0 eventlet==0.30.2
 # pip install -r /home/flask_website/workspace/flask_website/requirements.txt
 
 

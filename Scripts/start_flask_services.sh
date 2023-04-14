6857f09ba1355f5767236de72ff68f292b8266c9
Peter Paprotny <pappet.kn@gmail.com>
2023-04-14 23:21:04 +0200

    Changes

--- Scripts/start_flask_services.sh
+++ Scripts/start_flask_services.sh
@@ -21,15 +21,15 @@ echo "Starting Gunicorn..."
 nohup gunicorn --worker-class eventlet -w 1 wsgi:app --access-logfile /home/jenkins/workspace/flask_website/logs/gunicorn_access.log --error-logfile /home/jenkins/workspace/flask_website/logs/gunicorn_error.log --log-level info -b 0.0.0.0:8000 &
 
 # Wait for Gunicorn to start
-sleep 5
+# sleep 5
  
 # Check if Gunicorn is running
-GUNICORN_PID=$(pgrep -f "gunicorn")
+# GUNICORN_PID=$(pgrep -f "gunicorn")
 
-if [ -n "$GUNICORN_PID" ]; then
-    echo "Gunicorn is running (PID: $GUNICORN_PID)"
-else
-    echo "Gunicorn failed to start. Aborting script."
-    echo "(PID: $GUNICORN_PID)"
-    exit 1
-fi
+# if [ -n "$GUNICORN_PID" ]; then
+#     echo "Gunicorn is running (PID: $GUNICORN_PID)"
+# else
+#     echo "Gunicorn failed to start. Aborting script."
+#     echo "(PID: $GUNICORN_PID)"
+#     exit 1
+# fi

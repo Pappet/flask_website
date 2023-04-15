e9b3d4eb2f950356752e9a30b69a438c98cad95e
Peter Paprotny <pappet.kn@gmail.com>
2023-04-15 22:01:46 +0200

    Adding Login and Users

--- Application/app.py
+++ Application/app.py
@@ -1,17 +1,61 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
 from flask_socketio import SocketIO
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mysecretkey'
+app.config['SECRET_KEY'] = os.urandom(24)
+login_manager = LoginManager()
+login_manager.init_app(app)
 socketio = SocketIO(app)
+login_manager.login_view = 'login'
 
 
 @app.route('/')
+@login_required
 def index():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username in users and users[username]['password'] == password:
+            user = User(username)
+            login_user(user)
+            return redirect(url_for('index'))
+        else:
+            return 'Invalid username or password'
+    return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 
 @socketio.on('send_message')
 def handle_send_message(message):
     print('received message:', message)
     socketio.emit('message', message)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
+
+
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
+
+
+# Dummy users data
+users = {
+    'user1': {'password': 'password1'},
+    'user2': {'password': 'password2'}
+}

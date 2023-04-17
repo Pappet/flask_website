0ebcc35d5a602b5428acef09401786c6cfcd5e66
Peter Paprotny <pappet.kn@gmail.com>
2023-04-17 22:09:16 +0200

    Changed Start and Stop Services

--- Scripts/start_flask_services.sh
+++ Scripts/start_flask_services.sh
@@ -1,35 +1,54 @@
 #!/bin/bash
 
-# Start Nginx
-systemctl start nginx
-
 # Change to directory of flask app
 cd /home/jenkins/workspace/flask_website/Application
 
 # Install requirements
 pip install -r /home/jenkins/workspace/flask_website/requirements.txt
 
-export FLASK_APP=app.py  # On Linux or macOS
+# Define enviroment variable
+export FLASK_APP=app.py
 
 flask db init
 flask db migrate -m "Initial migration"
 flask db upgrade
 
-# Start Gunicorn
-echo "Starting Gunicorn..."
-# gunicorn --worker-class eventlet -w 1 wsgi:app --access-logfile /home/jenkins/workspace/flask_website/logs/gunicorn_access.log --error-logfile /home/jenkins/workspace/flask_website/logs/gunicorn_error.log --log-level info -b 0.0.0.0:8000 --daemon
-nohup gunicorn --worker-class eventlet -w 1 wsgi:app --access-logfile /home/jenkins/workspace/flask_website/logs/gunicorn_access.log --error-logfile /home/jenkins/workspace/flask_website/logs/gunicorn_error.log --log-level info -b 0.0.0.0:8000 &
+# --------------------------------------------------------------------------
+
+# Start Nginx
+echo "Starting nginx..."
+systemctl start nginx
 
 # Wait for Gunicorn to start
-# sleep 5
- 
-# Check if Gunicorn is running
-# GUNICORN_PID=$(pgrep -f "gunicorn")
+sleep 10
 
-# if [ -n "$GUNICORN_PID" ]; then
-#     echo "Gunicorn is running (PID: $GUNICORN_PID)"
-# else
-#     echo "Gunicorn failed to start. Aborting script."
-#     echo "(PID: $GUNICORN_PID)"
-#     exit 1
-# fi
+# Check the status of the Gunicorn service
+NGINX_STATUS=$(systemctl is-active nginx)
+
+# Print the status of the Gunicorn service
+if [ "$NGINX_STATUS" = "active" ]; then
+    echo "Nginx service is active and running"
+else
+    echo "Nginx service is not active"
+    exit 1
+fi
+
+# --------------------------------------------------------------------------
+
+# Start Gunicorn
+echo "Starting Gunicorn..."
+systemctl start gunicorn
+
+# Wait for Gunicorn to start
+sleep 10
+
+# Check the status of the Gunicorn service
+GUNICORN_STATUS=$(systemctl is-active gunicorn)
+
+# Print the status of the Gunicorn service
+if [ "$GUNICORN_STATUS" = "active" ]; then
+    echo "Gunicorn service is active and running"
+else
+    echo "Gunicorn service is not active"
+    exit 1
+fi
--- Scripts/stop_flask_services.sh
+++ Scripts/stop_flask_services.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Find the Gunicorn master process PID
-GUNICORN_MASTER_PID=$(pgrep -f "gunicorn" | sort -n | head -n 1)
+systemctl stop nginx
 
-# If the process was found, kill it
-if [ -n "$GUNICORN_MASTER_PID" ]; then
-    echo "Stopping Gunicorn (PID: $GUNICORN_MASTER_PID)..."
-    kill "$GUNICORN_MASTER_PID"
-    echo "Gunicorn stopped"
-else
-    echo "Gunicorn not running or not found"
-fi
+systemctl stop gunicorn
